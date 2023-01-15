2c42ed307f035827d45b255949db6e045dd49e58
huang-hua1212 <hahahazzz123@gmail.com>
2023-01-15 22:54:14 +0800

    add readDocx

---
+++ js-router/views/readDocx/index.php
@@ -0,0 +1,47 @@
+<!-- <!DOCTYPE html> -->
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+		<!-- CSS only -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">		
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+		<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" />
+  <title>
+  </title>
+</head>
+<body>
+</body>
+<script type = 'module'>
+     $.ajax({
+            url: 'http://localhost:8888/MAMP/projects/jsrouter-phpapi-datatable/router.php/read-single-docx',
+            async: true,
+            method: 'GET', 
+            cache: false,
+            success: function (res) {
+                console.log(res);
+
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+              console.log(jqXHR);
+                /*弹出jqXHR对象的信息*/
+                // alert(jqXHR.responseText);
+                // alert(jqXHR.status);
+                // alert(jqXHR.readyState);
+                // alert(jqXHR.statusText);
+                // /*弹出其他两个参数的信息*/
+                // alert(textStatus);
+                // alert(errorThrown);
+            }
+          });
+</script>
+</html>
--- router.php
+++ router.php
@@ -445,6 +445,14 @@ Route::add('/read-single-excel', function() {
 
 
 
+Route::add('/read-single-docx', function() {
+  // echo json_encode(csvToArray("./media/excel/csv/sample.csv"));
+  $fileName = "./media/word/test.docx";
+  echo readDocx($fileName);
+}, ['get','post']);
+
+
+
 
 
 
@@ -525,6 +533,35 @@ function csvToArray($importPath) {
     // print_r($row);
   }
   return $data;
+}
+
+
+function readDocx($filename){
+
+    $striped_content = '';
+    $content = '';
+
+    if(!$filename || !file_exists($filename)) return false;
+
+    $zip = zip_open($filename);
+    if (!$zip || is_numeric($zip)) return false;
+
+    while ($zip_entry = zip_read($zip)) {
+
+        if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
+
+        if (zip_entry_name($zip_entry) != "word/document.xml") continue;
+
+        $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+
+        zip_entry_close($zip_entry);
+    }
+    zip_close($zip);      
+    $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+    $content = str_replace('</w:r></w:p>', "\r\n", $content);
+    $striped_content = strip_tags($content);
+
+    return $striped_content;
 }
 
 
